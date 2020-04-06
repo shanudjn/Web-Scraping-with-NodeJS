cd2e3947eac6867a68979bc795d075f372224fd4
Shahazad <shanudjn>
2020-04-06 12:32:22 +0530

    added code that scrolls the page to get all reqd. links

--- scrape.js
+++ scrape.js
@@ -1,48 +1,93 @@
-let puppeteer = require('puppeteer')
-let cheerio = require('cheerio')
-var page ;
+let puppeteer = require("puppeteer");
+let cheerio = require("cheerio");
+let scroll = require("puppeteer-autoscroll-down");
 
-const EMAIL_SELECTOR = '#username';
-const PASSWORD_SELECTOR = '#password';
-const SUBMIT_SELECTOR = '#app__container > main > div > form > div.login__form_action_container > button';
-const LINKEDIN_LOGIN_URL = 'https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin';
-const SEARCH_STRING = process.argv[2]+" "+process.argv[3]+" "+process.argv[4] ;
+const EMAIL_SELECTOR = "#username";
+const PASSWORD_SELECTOR = "#password";
+const SUBMIT_SELECTOR =
+  "#app__container > main > div > form > div.login__form_action_container > button";
+const LINKEDIN_LOGIN_URL =
+  "https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin";
+const SEARCH_STRING =
+  process.argv[2] + " " + process.argv[3] + " " + process.argv[4];
+var links = [];
+
+var page;
+
+const autoScroll = async page => {
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      let totalHeight = 0;
+      let distance = 100;
+      let timer = setInterval(() => {
+        let scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+};
+
+//main
 if (process.argv[2] !== undefined) {
-    (() => {
-        puppeteer.launch({ headless: false })
-            .then(async (browser) => {
-                page = await browser.newPage()
-                page.setViewport({ width: 1366, height: 768 });
-                await page.goto(LINKEDIN_LOGIN_URL, { waitUntil: 'domcontentloaded' })
-                await page.click(EMAIL_SELECTOR)
-                await page.keyboard.type('shanudjn@gmail.com');
-                await page.click(PASSWORD_SELECTOR);
-                await page.keyboard.type('Shahazad@123');
-                await page.click(SUBMIT_SELECTOR);
-                
-                await page.goto(`https://www.linkedin.com/search/results/people/?keywords=${SEARCH_STRING}&origin=CLUSTER_EXPANSION`, { waitUntil: 'domcontentloaded' })
-                var data  = await getPage(); 
-                console.log(data);                      
-                    })                                
-                    
-            .catch((err) => {
-                console.log(" CAUGHT WITH AN ERROR ", err);
-            })
-    })();
-    //console.log(data);
-  function getPage(){
-    const content = page.content();    
-    return content.then((success) => {                               
-            const $ = cheerio.load(success)
-            const div = $('.search-results__list');                                
-            console.log(div.attr('class'));
-            var targetUrl = div.children().children().children().children('.search-result__info', '.pt3', '.pb4', '.ph0').children();
-            //var targetUrl = div.('li:nth-child(3)')                                
-            console.log(targetUrl.attr('class'));
-            console.log(targetUrl.attr('href'));           
-            
-            return targetUrl.attr('class');                                
-        })
-    //return hello;            
-  }
+  (() => {
+    puppeteer
+      .launch({ headless: false })
+      .then(async browser => {
+        page = await browser.newPage();
+        page.setViewport({ width: 1366, height: 768 });
+        await page.goto(LINKEDIN_LOGIN_URL, { waitUntil: "domcontentloaded" });
+
+        await page.click(EMAIL_SELECTOR);
+        await page.keyboard.type("your@email.com");
+        await page.click(PASSWORD_SELECTOR);
+        await page.keyboard.type("yourpassword");
+        await page.click(SUBMIT_SELECTOR);
+
+        await page.goto(
+          `https://www.linkedin.com/search/results/people/?keywords=${SEARCH_STRING}&origin=CLUSTER_EXPANSION`,
+          { waitUntil: "domcontentloaded" }
+        );
+
+        await autoScroll(page);
+
+        var data = await getPage();
+
+        for (let i = 0; i < data.length; i++) {
+          console.log("Link " + i + " : " + data[i]);
+        }
+      })
+      .catch(err => {
+        console.log(" CAUGHT WITH AN ERROR ", err);
+      });
+  })();
+}
+
+async function getPage() {
+  const content = page.content();
+
+  return content.then(success => {
+    const $ = cheerio.load(success);
+    let divClass = $(".search-results__list").find("li");
+    //console.log(divClass.attr('class'));
+    let listLength = divClass.length;
+    //console.log(listLength);
+
+    divClass.each(function(i, element) {
+      const targetUl = $(element).find(".search-result__info,.pt3, .pb4, .ph0");
+      //console.log(targetUl.attr('class'));
+      const name = $(targetUl)
+        .find(".name, .actor-name")
+        .text();
+      const link = $(targetUl)
+        .find("a")
+        .attr("href");
+      links.push(link);
+    });
+    return links;
+  });
 }
